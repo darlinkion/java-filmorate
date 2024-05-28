f51cc24c85e314ebd2bdf421ef814f36eadd468a
Darlinkion <htdhytfhyfmtftyftmy@yandex.ru>
2024-05-28 20:56:28 +0300

    Убрал лишний импорт.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.*;
-import lombok.experimental.SuperBuilder;
 import ru.yandex.practicum.filmorate.validator.FilmReleaseDate;
 
 import java.time.LocalDate;
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import lombok.*;
-import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
 import java.util.HashSet;
