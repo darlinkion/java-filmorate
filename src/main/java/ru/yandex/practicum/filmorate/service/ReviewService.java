1e4e0a5c53b7054b76030deaffbe2ed3f6527706
Darlinkion <htdhytfhyfmtftyftmy@yandex.ru>
2024-06-23 23:19:52 +0300

    Rebase с develop, после влития двух фич

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -1,10 +1,16 @@
 package ru.yandex.practicum.filmorate.service;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.repository.JdbcReviewRepository;
 
 import java.util.List;
-
+@Service
+@RequiredArgsConstructor
 public class ReviewService {
+    private final JdbcReviewRepository jdbcReviewRepository;
 
     public Review create(Review temp) {
         return null;
@@ -15,13 +21,31 @@ public class ReviewService {
         return null;
     }
 
+    public void deleteById(int id){
 
-    public List<Review> getAll() {
-        return null;
     }
-
 
     public Review get(int id) {
         return null;
+    }
+
+    public List<Review> getAll(Integer filmId, Integer count) {
+        return null;
+    }
+
+    public void setLike(int id, int userId){
+
+    }
+
+    public void setDislike(int id, int userId){
+
+    }
+
+    public void removeLike(int id, int userId){
+
+    }
+
+    public void removeDislike(int id, int userId){
+
     }
 }
