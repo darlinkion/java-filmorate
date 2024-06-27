9705ea2ef9223c50e423105dcdf2d9134a8b6b0b
darlinkion <htdhytfhyfmtftyftmy@yandex.ru>
2024-06-27 15:38:41 +0300

    Add reviews (#12)
    * Зачятки фичи
    
    * Добавил таблицы, начал желать репозиторий отзывов.
    
    * Зачятки фичи
    
    * Rebase с develop, после влития двух фич
    
    * Добавил пару методов в репозиторий отзывов, исправил опечатку в базе
    
    * Зачятки фичи
    
    * Rebase с develop, после влития двух фич
    
    * Добавил пару методов в репозиторий отзывов, исправил опечатку в базе
    
    * Зачятки методов
    
    * Добавил 1 метод, поправил схему, добавил удаление отзыва каскадом, при удалении фильма, пользователя, отзыва
    
    * Написал метод подсчета лайков.
    
    * Поправил ошибки
    
    * Поправил ошибки чекстайла
    
    * Поправил ошибки реализации
    
    * Поправил ошибки реализации
    
    * Поправил ошибки
    
    * Поправил ошибки
    
    * Очистил кэш
    
    * Исправил логику лайков.
    
    * Исправил логику лайков.

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -0,0 +1,64 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.repository.JdbcFilmIRepository;
+import ru.yandex.practicum.filmorate.repository.JdbcReviewRepository;
+import ru.yandex.practicum.filmorate.repository.JdbcUserRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+    private final JdbcReviewRepository jdbcReviewRepository;
+    private final JdbcUserRepository jdbcUserRepository;
+    private final JdbcFilmIRepository jdbcFilmIRepository;
+
+    public Review create(Review temp) {
+        jdbcUserRepository.get(temp.getUserId());
+        jdbcFilmIRepository.get(temp.getFilmId());
+        return jdbcReviewRepository.createReview(temp);
+    }
+
+    public Review update(Review temp) {
+        //get(temp.getReviewId());
+        return jdbcReviewRepository.update(temp);
+    }
+
+    public void deleteById(int id) {
+        get(id);
+        jdbcReviewRepository.deleteById(id);
+    }
+
+    public Review get(int id) {
+        return jdbcReviewRepository.get(id);
+    }
+
+    public List<Review> getAll(Integer filmId, Integer count) {
+        if (filmId == null) {
+            return jdbcReviewRepository.getAll(count);
+        } else {
+            return jdbcReviewRepository.getAllByFilmId(filmId, count);
+        }
+    }
+
+    public void setLike(int reviewId, int userId) {
+        jdbcUserRepository.get(userId);
+        get(reviewId);
+        jdbcReviewRepository.setLike(reviewId, userId);
+    }
+
+    public void setDislike(int reviewId, int userId) {
+        jdbcUserRepository.get(userId);
+        get(reviewId);
+        jdbcReviewRepository.setDislike(reviewId, userId);
+    }
+
+    public void removeGrade(int reviewId, int userId) {
+        get(reviewId);
+        jdbcUserRepository.get(userId);
+        jdbcReviewRepository.removeGrade(reviewId, userId);
+    }
+}
