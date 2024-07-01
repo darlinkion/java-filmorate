5c8507c322135f30d0400dcaa77ad8dc0e1ea852
Матвей <matvej.oganezov@yandex.ru>
2024-07-01 20:13:13 +0300

    refactor: поменять тип ошибки при удалении users и films

--- src/main/java/ru/yandex/practicum/filmorate/repository/JdbcUserRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/JdbcUserRepository.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.repository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
@@ -85,9 +86,9 @@ public class JdbcUserRepository implements IRepository<User> {
         try {
             String sql = "delete from users where id = ?";
             jdbc.update(sql, id);
-        } catch (Exception e) {
+        } catch (EmptyResultDataAccessException e) {
             log.error("Ошибка при удалении пользователя из БД: {}", e.getMessage(), e);
-            throw new NotFoundException("Ошибка при удалении пользователя из БД:");
+            throw new NotFoundException("Нет такого пользователя по id = " + id);
         }
     }
 
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -51,9 +51,6 @@ public class UserService implements BaseService<User> {
 
     @Override
     public void deleteById(int id) {
-        if (get(id) == null) {
-            throw new NotFoundException("Нет такого пользователя по id " + id);
-        }
         jdbcUserRepository.deleteById(id);
     }
 
